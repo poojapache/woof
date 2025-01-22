7ad2ae63b075d83662b7723b3870cafde07844d6
poojapache <ppache113@gmail.com>
2025-01-21 22:14:40 -0500

    fix for warnings

--- src/container/DogSearchContainer/dogList/DogList.tsx
+++ src/container/DogSearchContainer/dogList/DogList.tsx
@@ -6,7 +6,7 @@ export default function DogList({ dogList }: { dogList: Dog[] }): JSX.Element {
     <div className="p-2 overflow-y-auto flex justify-center w-full h-full shadow-inner border-4 bg-white border-orange-500">
       <div className="flex flex-row flex-wrap justify-start h-fit p-10 gap-8 max-w-full">
         {dogList.map((dog) => (
-          <DogListItem dog={dog} />
+          <DogListItem dog={dog} key={dog.id} />
         ))}
       </div>
     </div>
